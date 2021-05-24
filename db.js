050cdf9cabe96ca73f9d580db3f1ec1b86ebce75
Margarita Danshina <margaritadanshina@Margaritas-MacBook-Air.local>
2021-05-24 17:21:57 -0400

    bcrypt

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const { STRING } = Sequelize;
 const config = {
   logging: false,
@@ -38,14 +39,15 @@ User.byToken = async (token) => {
 };
 
 User.authenticate = async ({ username, password }) => {
+  // console.log("HASH AUTH",hash)
   const user = await User.findOne({
     where: {
       username,
-      password,
     },
   });
+  const compare = bcrypt.compareSync(password, user.password);
   const token = jwt.sign({ userId: user.id }, "Margarita");
-  if (user) {
+  if (compare) {
     return token;
   }
   const error = Error("bad credentials");
@@ -55,22 +57,32 @@ User.authenticate = async ({ username, password }) => {
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
+  
   const credentials = [
     { username: "lucy", password: "lucy_pw" },
     { username: "moe", password: "moe_pw" },
     { username: "larry", password: "larry_pw" },
   ];
-  const [lucy, moe, larry] = await Promise.all(
-    credentials.map((credential) => User.create(credential))
-  );
-  return {
-    users: {
-      lucy,
-      moe,
-      larry,
-    },
-  };
-};
+ 
+    
+    const [lucy, moe, larry] = await Promise.all(
+      credentials.map(async (credential) => { 
+          const hash = await bcrypt.hash(credential.password, 10);
+          credential.password = hash;
+          User.create(credential)
+          // console.log("CREDENTIALS---", credential)
+          return credential
+        }
+      )
+    )
+        return {
+          users: {
+            lucy,
+            moe,
+            larry,
+          },
+        }
+      };
 
 module.exports = {
   syncAndSeed,
